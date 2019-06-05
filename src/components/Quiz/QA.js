7b4fc5936b6e60a132ac0c5b632cba2045a40858
DAMMAK <adekoyafelix@gmail.com>
2019-06-05 23:12:03 +0100

    New Updates on Quiz

--- src/components/Quiz/QA.js
+++ src/components/Quiz/QA.js
@@ -8,10 +8,13 @@ class QA extends Component {
     super();
     this.next = this.next.bind(this);
     this.previous = this.previous.bind(this);
+    this.pick = this.pick.bind(this);
+
     this.state = {
       questionData: [],
       current: 0,
-      ansQues: []
+      ansQues: {},
+      correctAns: 0
     };
   }
 
@@ -21,17 +24,94 @@ class QA extends Component {
     // this.setState({questionData:question.data.questions});
 
     this.setState({ questionData: qs.questions });
+
+    var element = document.querySelectorAll("input");
+    // console.log(element);
+    element.forEach(el => {
+      el.addEventListener("click", () => {
+        this.pick(el);
+      });
+    });
+  }
+
+  clearChecked() {
+    var element = document.querySelectorAll("input");
+    // console.log(element);
+    element.forEach(el => {
+      el.checked = false;
+      el.nextSibling.classList.remove("correct");
+      el.nextSibling.classList.remove("wrong");
+      el.disabled = false;
+    });
+  }
+  pick(event) {
+    this.clearChecked();
+    this.disableInput();
+    let keys = Object.keys(this.state.ansQues);
+    var val = event.value;
+    if (
+      keys.includes(this.state.current) === false &&
+      this.state.questionData[this.state.current].ans === val
+    ) {
+      this.state.ansQues[this.state.current] = { [val]: true };
+      this.setState({ correctAns: this.state.correctAns + 1 });
+      // event.nextSibling.style.backgroundColor='green';
+      // event.nextSibling.style.color='#fff';
+      event.nextSibling.classList.add("correct");
+    } else {
+      this.state.ansQues[this.state.current] = { [val]: false };
+      event.nextSibling.classList.add("wrong");
+    }
+    // console.log(this.state);
+  }
+
+  disableInput() {
+    var element = document.querySelectorAll("input");
+    // console.log(element);
+    element.forEach(el => {
+      el.disabled = true;
+    });
   }
 
   next() {
+        this.clearChecked();
+        let keys = Object.keys(this.state.ansQues);
+            console.log("keys", keys);
     if (this.state.current !== this.state.questionData.length - 1) {
-      this.setState({ ...this.state, current: this.state.current + 1 });
+      console.log(this.state);
+      let cur = this.state.current + 1;
+       console.log(cur);
+      if (keys.includes(cur.toString()) === true) {
+        var elkey=Object.keys(this.state.ansQues[cur]);
+        var mark = document.getElementById(elkey);
+        console.log('it in there',mark.nextSibling);
+        // mark.nextSibling.classList.add("correct");
+       this.state.ansQues[cur][elkey] ? mark.nextSibling.classList.add("correct") : mark.nextSibling.classList.add("wrong");
+        
+        this.disableInput();
+      }
+          this.setState({ ...this.state, current: this.state.current + 1 });
     }
+
   }
 
   previous() {
+    let keys = Object.keys(this.state.ansQues);
+    console.log("keys", keys);
+
     if (this.state.current > 0) {
       this.setState({ ...this.state, current: this.state.current - 1 });
+      // console.log(this.state);
+      let cur = this.state.current - 1;
+      this.clearChecked();
+      if (keys.includes(cur.toString()) === true) {
+        var elkey=Object.keys(this.state.ansQues[cur]);
+        var mark = document.getElementById(elkey);
+        console.log('',mark.nextSibling);
+       this.state.ansQues[cur][elkey] ? mark.nextSibling.classList.add("correct") : mark.nextSibling.classList.add("wrong");
+        
+        this.disableInput();
+      }
     }
   }
   render() {
@@ -44,28 +124,31 @@ class QA extends Component {
         </div>
         <div className="option">
           <div className="option__A">
-            <input type="radio" name="option" id="A" />
-            <label for="A">
+            <input type="radio" name="option" id="A" value="A" />
+            <label for="A" class="test">
               {this.state.questionData.length > 0
                 ? this.state.questionData[this.state.current].options["A"]
                 : ""}
             </label>
           </div>
           <div className="option__B">
-            <input type="radio" name="option" id="B" />
+            <input type="radio" name="option" id="B" value="B" />
             <label for="B">
-            {this.state.questionData.length > 0
-              ? this.state.questionData[this.state.current].options["B"]
-              : ""}
-              </label>
+              {this.state.questionData.length > 0
+                ? this.state.questionData[this.state.current].options["B"]
+                : ""}
+            </label>
           </div>
-
         </div>
 
         <div className="navigation">
           <button className="navigation__previous" onClick={this.previous}>
             Previous Question
           </button>
+          <span>
+            Question: {this.state.current + 1} /{" "}
+            {this.state.questionData.length}{" "}
+          </span>
           <button className="navigation__next" onClick={this.next}>
             Next Question
           </button>
